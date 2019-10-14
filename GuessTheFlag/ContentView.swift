c456c2cbddb92e30dfce98955539fb4c82878a13
Mihail87 <leontemihai@gmail.com>
2019-10-14 11:34:28 +0300

    Day 20 completed

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    let size: CGFloat = 60
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello World")
+        
+        
+        ZStack {
+            //Color.gray.edgesIgnoringSafeArea(.all)
+            //LinearGradient(gradient: Gradient(colors: [Color.white, Color.gray]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            //RadialGradient(gradient: Gradient(colors: [Color.gray, Color.white]), center: .top, startRadius: 20, endRadius: 400).edgesIgnoringSafeArea(.all)
+            AngularGradient(gradient: Gradient(colors: [Color.white, Color.black]), center: .topTrailing).edgesIgnoringSafeArea(.all)
+
+            VStack {
+                
+                
+                Button(action: {
+                    self.showingAlert = true
+                }) {
+                    HStack {
+                        Image(systemName: "pencil.circle")
+                        Text("Edit")
+                    }
+                }.alert(isPresented: $showingAlert) { 
+                    Alert(title: Text("Edit Mode"), message: Text("You have entered edit mode."), dismissButton: .default(Text("OK")))
+                }
+                
+                HStack {
+                    Color(red: 1, green: 0.8, blue: 0, opacity: 0.3).frame(width: size, height: size)
+                    Color.blue.frame(width: size, height: size)
+                    Color.blue.frame(width: size, height: size)
+                }
+                HStack {
+                    Color.blue.frame(width: size, height: size)
+                    Color.blue.frame(width: size, height: size)
+                    Color.blue.frame(width: size, height: size)
+                }
+                HStack {
+                    Color.blue.frame(width: size, height: size)
+                    Color.blue.frame(width: size, height: size)
+                    Color.blue.frame(width: size, height: size)
+                }
+            }
+        }
     }
 }
 
